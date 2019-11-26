b73be0279851a4e82a8568f5c66fe285c105c9b3
Iryna Dudarieva <irenkane@gmail.com>
2019-11-26 11:35:12 +0200

    Added new request "currency".

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 //1. Написать сервер, чтоб по запросу localhost:3000 сервер выдавал нам в response Hello World,
 // чтоб по запросу localhost:3000/about сервер выдавал нам данные о запросе в консоль,
 // а по запросу localhost:3000/contact сервер возвращал страницу index.html.
+//*3. По этой ссылке находятся курсы валют Приват Банка в виде json. Задание: попробовать с помощью Node,
+// отобразить эти данные у себя в консоли или же вывести в ответ на запрос сервера (localhost:3000/currency).
 let http = require('http');
 let port = 3000;
 let fs = require('fs');
 let indexFileContent = fs.readFileSync('index.html');
+let https = require('https');
 
 http.createServer(function (req, res) {
     if (req.url === '/') {
@@ -17,6 +20,20 @@ http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': indexFileContent.length});
         res.write(indexFileContent);
         res.end();
+    } else if (req.url === '/currency') {
+        https.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR2ym_1JdP9amiJLliYaB96SRCQkPbj_FQKllz5PYQzMWb04Zl9hO0RQlnc', (resp) => {
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            resp.on('end', () => {
+                console.log(JSON.stringify(data));
+                res.write(data);
+                res.end();
+            });
+
+        });
+
     }
 }).listen(port, function () {
     console.log('Server at http://localhost:3000')
